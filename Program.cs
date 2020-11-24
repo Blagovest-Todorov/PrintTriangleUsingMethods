7c9be96c7b4764a74d5339c4f076f1755282d97e
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-24 20:55:07 +0200

    change-making the method more universal

--- Program.cs
+++ Program.cs
@@ -8,18 +8,18 @@ namespace _05.PrintTriangle
         {
             Console.Write("n = ");
             int n = int.Parse(Console.ReadLine());  //Lets print a triangle using methods
-            for (int line = 1; line <= n ; line++)  //n-> number of lines to print
+            for (int line = 1; line <= n; line++)  //n-> number of lines to print
             {
-                PrintLine(1, line); // 1 ->start; line-> end //we just replace Pair(1/line) ->(start/end) into Method
+                PrintLine(10, line); // 1 ->start; line-> end //we just replace Pair(1/line) ->(start/end) into Method
             }
             for (int line = n - 1; line >= 1; line--)
             {
-                PrintLine(1, line);
+                PrintLine(10, line);
             }
 
-            static void PrintLine(int start, int end) 
+            static void PrintLine(int start, int length)
             {
-                for (int i = start; i <= end; i++)
+                for (int i = start; i < start + length; i++)
                 {
                     Console.Write(" {0}", i);
 
