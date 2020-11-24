3fbe4ed9287ff93a1f2e057363c6af3575cc69f5
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-24 20:23:11 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.PrintTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());  //Lets print a triangle using methods
+            for (int line = 1; line <= n ; line++)  //n-> number of lines to print
+            {
+                PrintLine(1, line); // 1 ->start; line-> end //we just replace Pair(1/line) ->(start/end) into Method
+            }
+            for (int line = n - 1; line >= 1; line--)
+            {
+                PrintLine(1, line);
+            }
+
+            static void PrintLine(int start, int end) 
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    Console.Write(" {0}", i);
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
